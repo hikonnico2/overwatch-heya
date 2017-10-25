0b35f8634f4de7764d10b5b7c2e1e32630321218
hikon <hikonnico2@gmail.com>
2017-10-25 09:36:28 +0000

    ファーストコミット

---
+++ top.js
@@ -0,0 +1,29 @@
+(() => {
+  'use strict';
+  
+  let refOffset = 0;
+  const bannerHeight = 90;
+  const bannerWrapper = document.querySelector('.banner-wrapper');
+  const banner = document.querySelector('.banner');
+
+  const handler = () => {
+    const newOffset = window.scrollY || window.pageYOffset;
+
+    if (newOffset > bannerHeight) {
+      if (newOffset > refOffset) {
+        bannerWrapper.classList.remove('animateIn');
+        bannerWrapper.classList.add('animateOut');
+      } else {
+        bannerWrapper.classList.remove('animateOut');
+        bannerWrapper.classList.add('animateIn');
+      }
+      banner.style.background = 'rgba(162, 197, 35, 0.6)';
+      refOffset = newOffset;
+    } else {
+      banner.style.backgroundColor = 'rgba(227, 253, 112, 0.9)'; /*rgba(162, 197, 35, 1)*/
+    }
+  };
+
+  window.addEventListener('scroll', handler, false);
+
+})();
